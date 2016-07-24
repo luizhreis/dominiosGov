b26cc1f38354191d498942646ef4cfec096622e3
beatriza <bia.andradec@gmail.com>
2016-07-24 03:21:32 -0300

    add a db 'original' e a db alterada. utilizar a criar_database_dominiosgov2

--- scripts/criar_database_original.sql
+++ scripts/criar_database_original.sql
@@ -5,11 +5,18 @@ bandeira blob,
 sigla char(2) NOT NULL
 );
 
-CREATE TABLE Cidades(
+CREATE TABLE Cidades (
 cidade varchar(128),
 id_cidade serial PRIMARY KEY,
 id_estado bigint unsigned,
 FOREIGN KEY(id_estado) REFERENCES Estados (id_estado) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Telefones (
+tipo varchar(16),
+ddd numeric(3),
+numero numeric(9),
+PRIMARY KEY(ddd,numero)
 );
 
 CREATE TABLE Nservers (
@@ -29,18 +36,8 @@ CREATE TABLE Ents_Resps (
 documento varchar(32) PRIMARY KEY,
 cep char(9),
 nome varchar(128),
-id_cidade serial,
+id_cidade bigint unsigned,
 FOREIGN KEY(id_cidade) REFERENCES Cidades (id_cidade) ON UPDATE CASCADE ON DELETE RESTRICT
-);
-
-CREATE TABLE Telefones (
-tipo varchar(16),
-ddd numeric(3),
-numero numeric(9),
-documento varchar(32),
-PRIMARY KEY(ddd,numero),
-FOREIGN KEY(documento) REFERENCES Ents_Resps (documento) ON UPDATE CASCADE ON DELETE CASCADE
-/*falha: chave estrangeira*/
 );
 
 CREATE TABLE Funcionario (
@@ -55,7 +52,8 @@ CREATE TABLE Dominios (
 data_cadastro date,
 ultima_atualizacao date,
 domain varchar(255) PRIMARY KEY,
-ticket numeric(9),
+ip varchar(16),
+ticket varchar(16),
 documento varchar(32),
 FOREIGN KEY(documento) REFERENCES Ents_Resps (documento) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
@@ -77,5 +75,4 @@ PRIMARY KEY(tipo,nic_hdl_br,domain),
 FOREIGN KEY(nic_hdl_br) REFERENCES NicHandles (nic_hdl_br) ON UPDATE CASCADE ON DELETE RESTRICT,
 FOREIGN KEY(domain) REFERENCES Dominios (domain) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 
